11f461d6289cfde9682a247bda9d4003ed7f1aaf
John M. Harris, Jr <johnmh@openblox.org>
2017-08-02 18:36:06 -0400

    Properly check for definitions in chipone.h

--- chipone.h
+++ chipone.h
@@ -5,15 +5,15 @@
 #define CHIPONE_DRIVER_NAME    "chipone_ts"
 #define CHIPONE_NAME           "CHPN0001"
 
-#ifdef CONFIG_VI8PLUS
+#ifdef defined(CONFIG_VI8PLUS)
     #define SCREEN_MAX_X 1920
     #define SCREEN_MAX_Y 1200
     #define CHIPONE_IRQ  0x5C // HACK: Hardcode IRQ, kernel doesn't get it at boot time
-#elif CONFIG_CW1515
+#elif defined(CONFIG_CW1515)
 	#define SCREEN_MAX_X 1920
 	#define SCREEN_MAX_Y 1200
 	#define CHIPONE_IRQ 0xB8
-#elif CONFIG_HI10
+#elif defined(CONFIG_HI10)
     #define SCREEN_MAX_X 1200
     #define SCREEN_MAX_Y 1900
     #define CHIPONE_IRQ  0xB9 // HACK: Hardcode IRQ, kernel doesn't get it at boot time
