33aa468e3c35fd60caf23ba9f984d664e1b0f2d8
John M. Harris, Jr <johnmh@openblox.org>
2017-08-03 11:20:40 -0400

    Fix code style in chipone_types.h

--- chipone_types.h
+++ chipone_types.h
@@ -11,10 +11,11 @@
 struct chipone_ts_data{
 	unsigned int irq;
 	struct i2c_client* client;
-	struct input_dev *input;
+	struct input_dev* input;
 	struct timeval last_irq_event;
 	struct chipone_ts_header_area_regs last_header_area;
 	struct chipone_ts_coordinate_area_regs last_coordinate_area;
+	unsigned long lastSuperPress;
 };
 
 #endif // CHIPONE_TYPES_H
