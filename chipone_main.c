91e52b1022e7478c29bb358130edcb01704b3cfe
John M. Harris, Jr <johnmh@openblox.org>
2017-08-03 11:05:32 -0400

    Debug print for the sake of sanity

--- chipone_main.c
+++ chipone_main.c
@@ -184,6 +184,8 @@ static int chipone_ts_probe(struct i2c_client* client, const struct i2c_device_i
 		err = chipone_ts_regs_set_resolution(client, screen_max_x, screen_max_y);
 		if(err < 0){
 			dev_warn(dev, "Failed to set screen resolution, trying again.\n");
+		}else{
+			dev_info(dev, "Set resolution");
 		}
 	}
 	if(err < 0){
